0a4c46be53f00e9a8ce4da12005e78e283610e58
Paulo Fernandes <paulor.exe@gmail.com>
2025-03-03 19:30:32 -0300

    feat: add card component variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ import { PackageSearch } from 'lucide-react'
 export default function Home() {
   return (
     <div className="-mt-16 flex h-screen w-full items-center justify-center gap-4">
-      {/* <Card
+      <Card
         icon={PackageSearch}
         title="Produtização"
         text="Somos o seu parceiro para a transformação digital. Independente do seu negócio, entregamos seu produto, app ou dashboard personalizados para as suas necessidades."
-      /> */}
+      />
+      <Card
+        variant="secondary"
+        icon={PackageSearch}
+        title="Produtização"
+        text="Somos o seu parceiro para a transformação digital. Independente do seu negócio, entregamos seu produto, app ou dashboard personalizados para as suas necessidades."
+      />
     </div>
   )
 }
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,16 +1,29 @@
 import { LucideIcon } from 'lucide-react'
 
 interface CardProps {
+  variant?: 'primary' | 'secondary'
   icon: LucideIcon
   title: string
   text: string
 }
 
-export function Card({ icon: Icon, title, text }: CardProps) {
+export function Card({
+  icon: Icon,
+  title,
+  text,
+  variant = 'primary',
+}: CardProps) {
+  const cardStyles =
+    variant === 'primary'
+      ? 'border border-zinc-600 bg-linear-150 backdrop-blur-2xl from-[#292932]/60 hover:shadow-slate-800/60 from-45% to-[#A7BFFC]/30'
+      : 'bg-white hover:shadow-zinc-800/60'
+
   return (
-    <div className="relative flex min-h-96 max-w-[400] flex-col gap-6 rounded-2xl border border-zinc-600 bg-linear-150 from-[#292932]/60 from-45% to-[#A7BFFC]/30 p-12 backdrop-blur-2xl transition hover:scale-[1.03] hover:shadow-2xl hover:shadow-slate-800/60 hover:transition">
+    <div
+      className={`relative flex min-h-96 max-w-[400] flex-col gap-6 rounded-2xl p-12 transition hover:scale-[1.03] hover:shadow-2xl hover:transition ${cardStyles}`}
+    >
       <Icon size={52} className="text-[#A7BFFC]" />
-      <h3 className="text-2xl text-[#A7BFFC]">{title}</h3>
+      <h3 className="text-2xl font-semibold text-[#A7BFFC]">{title}</h3>
       <p className="text-zinc-500">{text}</p>
     </div>
   )
